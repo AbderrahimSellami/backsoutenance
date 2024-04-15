7738d21b9f6bd2006d9a7dae9649d1ad2cc10693
karim.ammouche <karim.ammouche@mousquetaires.com>
2024-04-15 22:30:43 +0200

    changes

--- backsoutenance/authentification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('',views.authentification, name='authentification'),
-    path('form_submission', views.form_submission , name ='form_submission'),
-]
