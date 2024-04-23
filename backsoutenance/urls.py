f382c04e97604e7d9526a72f0df0e418a8928aa6
AbderrahimSellami <nadmehid@gmail.com>
2024-04-23 23:10:51 +0100

    Update urls.py

--- backsoutenance/urls.py
+++ backsoutenance/urls.py
@@ -4,4 +4,6 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentification/',include('authentification.urls'),name='authentification'),
+    path('planning/',include('planning.urls'),name='planning'),
+    path('utilisateurs/',include('utilisateurs.urls'),name='utilisateurs'),
 ]
